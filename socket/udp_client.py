fb2868a906328c0a108b7e75aa8c2cb35b07fea4
ignazio <ignazioemanuele.picciche@gmail.com>
2024-03-12 16:57:45 +0000

    aggiunti i commenti

--- socket/udp_client.py
+++ socket/udp_client.py
@@ -1,13 +1,20 @@
+# Import the socket library for network connections and sys for command line arguments
 import socket
 from sys import argv
 
+# Define the server IP and port
 ServerIP     = '127.0.0.1'
-ServerPORT   = int(argv[1])
-bufferSize   = 1024
+ServerPORT   = int(argv[1])  # Convert the first command line argument to an integer for the port
+bufferSize   = 1024  # Define the size of the buffer for incoming messages
 
+# Create a UDP socket
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
+# Send a message to the server
 UDPClientSocket.sendto(str.encode('Questo è il messaggio del Client'), (ServerIP, ServerPORT))
+
+# Receive a message from the server
 mess, addr = UDPClientSocket.recvfrom(bufferSize) 
 
+# Print the message from the server, decoding it from bytes to a string
 print('C: Messaggio da parte del server: {}'.format(mess.decode('utf-8')))
--- socket/udp_server.py
+++ socket/udp_server.py
@@ -1,19 +1,36 @@
+# Protocollo UDP non connection oriented (non connesso)
+# Basato sul concetto della "lettera"
+
 import socket
 from sys import argv
 
+# Local IP address to which the server is bound
 localIP     = '127.0.0.1'
+
+# Local port number to which the server is bound, taken from command line arguments
 localPORT   = int(argv[1])
+
+# Size of the buffer to be used in data reception
 bufferSize  = 1024
 
+# Create a UDP socket
+# AF_INET is the Internet address family for IPv4
+# SOCK_DGRAM is the socket type for UDP
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+# Bind the socket to the local IP and port
 UDPServerSocket.bind((localIP, localPORT))
 
 print("S: UDP SERVER UP AND RUNNING!")
 
+# Server is always listening
 while True:
+    # Receive message from client, also save client's address
     mess, addr = UDPServerSocket.recvfrom(bufferSize)
 
+    # Print the received message and client's address
     print('S: Messaggio ricevuto da {}'.format(addr))
     print('S: Messaggio: {}'.format(mess.decode('utf-8')))
 
+    # Send a response back to the client
     UDPServerSocket.sendto(str.encode('Grazie del messaggio!'), addr)
